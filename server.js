fc1656f6af9f772540079fd06e42bc8b738fc7d4
Giridhar <giridhar@Giridhars-MacBook.local>
2017-06-15 14:50:50 +0530

    Fix log issue

--- server.js
+++ server.js
@@ -85,5 +85,5 @@ var server = app.listen(app.get('port'), function () {
    var host = server.address().address
    var port = server.address().port
    
-   log.info("App listening at http://%s:%s", host, port)
+   //log.info("App listening at http://%s:%s", host, port)
 })
