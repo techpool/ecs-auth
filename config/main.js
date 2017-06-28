3938ef9d1489a056838ad0d5552b3502d661bd52
Giridhar <giridhar@Giridhars-MacBook.local>
2017-06-28 15:35:54 +0530

    Add configurations for devo,gamma,prod environments

--- config/main.js
+++ config/main.js
@@ -2,15 +2,24 @@ var config = {};
 
 config.devo = {
   'PORT': 80,
-  'SERVICE': 'devo-auth-aws',
+  'SERVICE': 'ecs-auth-devo',
   'REDIS_HOST_IP': 'ecs-auth.e6ocw5.0001.apse1.cache.amazonaws.com',
   'REDIS_HOST_PORT': '8080',
   'REDIS_KIND':'AUTH'
 };
 
+config.gamma = {
+  'PORT': 80,
+  'SERVICE': 'ecs-auth-gamma',
+  'REDIS_HOST_IP': 'ecs-auth.cpzshl.ng.0001.apse1.cache.amazonaws.com',
+  'REDIS_HOST_PORT': '8080',
+  'REDIS_KIND':'AUTH'
+};
+
+
 config.prod = {
   'PORT': 80,
-  'SERVICE': 'prod-auth-aws',
+  'SERVICE': 'ecs-auth-prod',
   'REDIS_HOST_IP': 'ecs-auth.cpzshl.ng.0001.apse1.cache.amazonaws.com',
   'REDIS_HOST_PORT': '8080',
   'REDIS_KIND':'AUTH'
