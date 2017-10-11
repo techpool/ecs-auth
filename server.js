5dca1ac842d6d4deb521fbfd782c7c6b936f2ea2
Giridhar Samanthipudi <samanth.giridhar@gmail.com>
2017-10-11 16:26:04 +0530

    Read access token from user-service

--- server.js
+++ server.js
@@ -667,22 +667,22 @@ function deleteFromCache (key) {
 }
 
 function getFromDB(accessToken, res) {
-	return AccessTokenService
-	.getUserId( accessToken )
-	.then( ( id ) => {
-		console.log("Reading user-id from gcp");
-
-		// add to cache
-		var user = new User(id);
-		cacheUtility.insert( accessToken, user );
-
-		res.setHeader('User-Id', id);
-		return id;
-	})
-	.catch( ( err ) => {
-		console.log(err);
-		return 0;
-	});
+	return userService
+ 	.getUserId( accessToken )
+ 	.then( ( id ) => {
+ 		console.log("Reading user-id from user service ");
+ 		
+ 		// add to cache
+ 		var user = new User(id);
+ 		cacheUtility.insert( accessToken, user );
+ 		
+ 		res.setHeader('User-Id', id);
+ 		return id;
+ 	})
+ 	.catch( ( err ) => {
+ 		console.log(err);
+ 		return 0;
+ 	});
 }
 
 
