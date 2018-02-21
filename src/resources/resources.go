86a9dfb6875c2ffe297766117b29cb1cc79e961a
Giridhar <samanth.giridhar@gmail.com>
2018-02-21 16:08:58 +0530

    Use redis config for env

--- src/resources/resources.go
+++ src/resources/resources.go
@@ -3,8 +3,10 @@ package resources
 import (
 	"github.com/go-redis/redis"
 
-	"auth/src/config"
 	"log"
+	"os"
+	"fmt"
+	"strconv"
 )
 
 var (
@@ -13,15 +15,24 @@ var (
 
 func Init() error {
 
+	redisHost := fmt.Sprintf("%s:%s",os.Getenv("MASTER_REDIS_ENDPOINT"),os.Getenv("MASTER_REDIS_PORT"))
+
+	redisDB, err := strconv.Atoi(os.Getenv("MASTER_REDIS_DB"))
+	if err != nil {
+		return err
+	}
+
 	log.Println("Initialising redis")
+	//log.Println(redisHost, redisDB)
+
 	Redis = redis.NewClient(&redis.Options{
-		Addr: config.Redis.Host,
-		DB: config.Redis.DB,
+		Addr: redisHost,
+		DB: redisDB,
 	});
 
-
-	_, err := Redis.Ping().Result()
+	_, err = Redis.Ping().Result()
 	if err != nil {
+		log.Println(err)
 		return err
 	}
 
