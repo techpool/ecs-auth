a58d9cac806176b587f48055719ebc09a5197b1a
Giridhar <samanth.giridhar@gmail.com>
2018-04-02 12:57:33 +0530

    Add delete message

--- src/utils/SQS.go
+++ src/utils/SQS.go
@@ -35,6 +35,8 @@ type InnerMessage struct {
 	AccessToken []string
 }
 
+var svc *sqs.SQS
+
 func SQSInit() {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(config.SQS.Region)},
@@ -44,8 +46,10 @@ func SQSInit() {
 		os.Exit(2)
 	}
 
-	svc := sqs.New(sess)
-
+	svc = sqs.New(sess)
+	receiveMessages()
+}
+func receiveMessages() {
 	ticker := time.NewTicker(time.Duration(config.SQS.PollIntervalSeconds) * time.Second)
 	log.Println(config.SQS.QueueURL)
 	for _ = range ticker.C {
@@ -70,21 +74,39 @@ func SQSInit() {
 	}
 }
 
+
 func processMessages(sqsMessages []*sqs.Message) {
 	for _,sqsMsg := range sqsMessages {
 		var sqsMessage SqsMessage
 		if err := json.Unmarshal([]byte(*sqsMsg.Body),&sqsMessage); err != nil {
 			log.Println("Error while unmarshaling error ",err)
 		}
-		log.Println(sqsMessage)
 		var message Message
 		if err := json.Unmarshal([]byte(sqsMessage.Message),&message); err != nil {
 			log.Println("Error while unmarshalling 2 error", err)
 		}
 
-		log.Println(message)
-		log.Println(message.Message)
-		log.Println(message.Message.AccessToken)
+		if message.Version == "2.0" && message.Event == "USER.DELETE" {
+		//log.Println(message.Message.AccessToken)
+			if len(message.Message.AccessToken) > 0 {
+				for _,at := range message.Message.AccessToken {
+					DeleteCache(at)
+				}
+			}
+		}
+		deleteMessage(sqsMsg.ReceiptHandle)
 	}
 }
 
+
+func deleteMessage(receiptHandle *string) {
+	_, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
+		QueueUrl:      &config.SQS.QueueURL,
+		ReceiptHandle: receiptHandle,
+	})
+
+	if err != nil {
+		log.Println("Delete Error", err)
+	}
+
+}
