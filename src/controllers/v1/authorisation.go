1b1554861c7c1a16b1fd6d6f982cd2ba78727896
Giridhar <samanth.giridhar@gmail.com>
2018-04-27 18:51:44 +0530

    Support for Content API

--- src/controllers/v1/authorisation.go
+++ src/controllers/v1/authorisation.go
@@ -15,7 +15,7 @@ import (
 	utilServices "auth/src/utils/services"
 )
 
-var validResources = []string{"/pratilipis","/authors","/recommendation/pratilipis","/search/search","/search/trending_search","/follows","/userauthor/follow/list", "/userauthor/follow","/reviews","/userpratilipi","/userpratilipi/review","/userpratilipi/review/list","/userpratilipi/reviews","/comments","/comment","/comment/list","/vote","/votes", "/blog-scraper","/event","/event/list","/events","/event/pratilipi","/devices", "/notifications","/userpratilipi/library","/userpratilipi/library/list","/library", "/social-connect","/user/register","/user/login","/user/login/facebook","/user/login/google","/user/verification","/user/email","/user/passwordupdate","/user","/user/logout","/authors/recommendation","/pratilipi/content/batch","/pratilipi/content/chapter/add","/pratilipi/content/chapter/delete","/pratilipi/content/index","/pratilipi/content","/coverimage-recommendation","/template-engine","/growthjava","/report","/init","/users/v2.0/admins/users/*","/admins/users","/events/v2.0","/user/firebase-token","/oasis","/user_pratilipi/v2.0/user_pratilipis","/blogs"}
+var validResources = []string{"/pratilipis","/authors","/recommendation/pratilipis","/search/search","/search/trending_search","/follows","/userauthor/follow/list", "/userauthor/follow","/reviews","/userpratilipi","/userpratilipi/review","/userpratilipi/review/list","/userpratilipi/reviews","/comments","/comment","/comment/list","/vote","/votes", "/blog-scraper","/event","/event/list","/events","/event/pratilipi","/devices", "/notifications","/userpratilipi/library","/userpratilipi/library/list","/library", "/social-connect","/user/register","/user/login","/user/login/facebook","/user/login/google","/user/verification","/user/email","/user/passwordupdate","/user","/user/logout","/authors/recommendation","/pratilipi/content/batch","/pratilipi/content/chapter/add","/pratilipi/content/chapter/delete","/pratilipi/content/index","/pratilipi/content","/coverimage-recommendation","/template-engine","/growthjava","/report","/init","/users/v2.0/admins/users/*","/admins/users","/events/v2.0","/user/firebase-token","/oasis","/user_pratilipi/v2.0/user_pratilipis","/blogs","/content"}
 
 var validMethods = []string{"POST","GET","PUT","PATCH","DELETE"}
 
@@ -627,6 +627,16 @@ func Validate(c echo.Context) error {
 		} else {
 			rpData = append(rpData,resourcePermission{403, 0, false})
 		}
+	} else if resource == "/content" {
+		if method == "GET" {
+			rpData = append(rpData,resourcePermission{200, 0, true})
+		} else {
+			if userId > 0 {
+				rpData = append(rpData,resourcePermission{200, 0, true})
+			} else {
+				rpData = append(rpData,resourcePermission{403, 0, false})
+			}
+		}
 	} else {
 		rpData = append(rpData,resourcePermission{403, 0, false})
 	}
@@ -806,6 +816,8 @@ func pathMapping(apiType string, c echo.Context) echo.Context {
                         resource == "/author-interviews/v1.0" || 
                         resource == "/author-interviews/v1.0/list" {
 			resource = "/blogs"
+		} else if resource == "/content/v1.0/contents/clipped" {
+			resource = "/content"
 		}
 
 		c.Set("originalResource",originalResource)
